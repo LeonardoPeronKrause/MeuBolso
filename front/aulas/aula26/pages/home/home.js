a257a7e235ccf624d7cbfe523eb930ca98c675c7
Leonardo Peron Krause <lperonkrause@gmail.com>
2024-10-11 13:04:41 -0300

    Adicionar arquivos do front

---
+++ front/aulas/aula26/pages/home/home.js
@@ -0,0 +1,119 @@
+const { error } = require("console");
+
+function logout() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = "../../index.html";
+    }).catch(() => {
+        alert('Erro ao fazer logout');
+    })
+}
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        user.getIdToken().then(token => console.log(token));
+        findTransactions(user);
+    }
+}) 
+
+function newTransaction() {
+    window.location.href = '../transaction/transaction.html'
+}
+
+findTransactions();
+
+function findTransactions(user) {
+    showLoading();
+    firebase.firestore()
+        .collection('transactions')
+        .where('user.uid', '==', user.uid)
+        .orderBy('date', 'desc')
+        .get()
+        .then(snapshot => {
+            hideLoading();
+            const transactions = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                uid: doc.uid
+            }));
+            addTransactionsToScreen(transactions);
+        })
+        .catch(error => {
+            hideLoading();
+            console.log(error);
+            alert('Erro ao recuperar transações');
+        })
+}
+
+function addTransactionsToScreen(transactions) {
+    const orderedList = document.getElementById('transactions');
+
+    transactions.forEach(transaction => {
+        const li = document.createElement('li');
+        li.classList.add(transaction.type);
+        li.id = transaction.uid;
+        li.addEventListener('click', () => {
+            window.location.href = '../transaction/transaction.html?uid=' + transaction.uid;
+        })
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = formatDate(transaction.date);
+        deleteButton.classList.add('outline', 'danger')
+        deleteButton.addEventListener('click', event => {
+            event.stopPropagation();
+            aksRemoveTransaction(transaction);
+        });
+        li.appendChild(deleteButton);
+
+        const date = document.createElement('p');
+        date.innerHTML = formatDate(transaction.date);
+        li.appendChild(date)
+
+        const money = document.createElement('p');
+        money.innerHTML = formatMoney(transaction.money);
+        li.appendChild(money);
+
+        const type = document.createElement('p');
+        type.innerHTML = transaction.transactionType;
+        li.appendChild(type);
+
+        if (transaction.description) {
+            const description = document.createElement('p');
+            description.innerHTML = transaction.description;
+            li.appendChild(description);
+        }
+
+        orderedList.appendChild(li);
+    });
+}
+
+function askRemoveTransaction() {
+    const shouldRemove = confirm('Deseja remover a transação?');
+    if (shouldRemove) {
+        removeTransaction(transaction);
+    }
+ }
+
+function removeTransaction(transaction) {
+    showLoading();
+
+    firebase.firestore()
+        .collection('transactions')
+        .doc(transaction.uid)
+        .delete()
+        .then(() => {
+            hideLoading();
+            document.getElementById(transaction.uid).remove();
+        })
+        .catch(error => {
+            hideLoading();
+            console.log(error);
+            alert('Erro ao remover transação')
+        })
+}
+
+function formatDate(date) {
+    return new Date(date).toLocaleDateString('pt-br');
+}
+
+function formatMoney(money) {
+    return `${money.currency} ${money.value.toFixed(2)}`
+}
